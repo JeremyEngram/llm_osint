b44e222ff4103ec2db6974adc0207fbeded6e912
NSR <65774401+JeremyEngram@users.noreply.github.com>
2023-10-20 01:33:46 -0400

    Update web_agent_constants.py

--- llm_osint/web_agent_constants.py
+++ llm_osint/web_agent_constants.py
@@ -1,49 +1,73 @@
-PREFIX = """
-You are a automated OSINT agent who can expertly find information on persons, companies, places, and things.
+import requests
+import os
+from bs4 import BeautifulSoup
+from typing import List, Optional
+from llm_osint import cache_utils
 
-You use the question/thought/action/observation format for performing these searches.
+MAX_LINK_LEN = 120
 
-If you have exhausted the available resources DO NOT STOP, perform another search for a detail you could use more information about.
+@cache_utils.cache_func
+def scrape_text(url: str, retries: Optional[int] = 2) -> str:
+    try:
+        resp = requests.get(
+            url="https://app.scrapingbee.com/api/v1/",
+            params={
+                "api_key": os.environ["SCRAPINGBEE_API_KEY"],
+                "url": url,
+                "premium_proxy": "true",
+                "country_code": "us",
+            },
+        )
+    except RuntimeError as e:
+        if retries > 0:
+            return scrape_text(url, retries=retries - 1)
+        else:
+            raise e
+    return resp.text
 
-Answer the users questions by scanning the internet for relevent data. Be very thorough.
+def extract_text_and_links(element) -> (str, List[str]):
+    elem_text = element.get_text()
+    lines = (line.strip() for line in elem_text.splitlines())
+    parts = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = "\n".join(c for c in parts if c)
 
-Hints:
-* Always read the contents of linked webpages that can give you more information to answer the query
-* Use the contents of webpages to recursively discover information. Crawl links that could provide more information.
+    links = [link["href"] for link in element.find_all("a", href=True) if len(link["href"]) <= MAX_LINK_LEN]
+    return text, list(set(links))
 
-You have access to the following tools:
-"""
+def chunk_element(element, max_size: int) -> List[str]:
+    text, links = extract_text_and_links(element)
+    if not text:
+        return []
+    if len(text) <= max_size:
+        return [text + "\n\nLinks: " + " ".join(links)]
+    else:
+        chunks = []
+        for child in element.findChildren(recursive=False):
+            chunks.extend(chunk_element(child, max_size))
+        return chunks
 
-FORMAT_INSTRUCTIONS = """The way you use the tools is by specifying a json blob.
-Specifically, this json should have a `action` key (with the name of the tool to use) and a `action_input` key (with the input to the tool going here).
+def merge_text_chunks(vals: List[str], max_size: int) -> List[str]:
+    combined_strings = []
+    current_string = ""
 
-The only values that should be in the "action" field are: {tool_names}
+    for s in vals:
+        if len(current_string) + len(s) <= max_size:
+            current_string += s
+        else:
+            combined_strings.append(current_string)
+            current_string = s
 
-The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. Here is an example of a valid $JSON_BLOB:
+    if current_string:
+        combined_strings.append(current_string)
 
-```
-{{{{
-  "action": $TOOL_NAME,
-  "action_input": $INPUT,
-}}}}
-```
+    return combined_strings
 
-ALWAYS use the following format:
+def chunk_and_strip_html(raw_html: str, max_size: int) -> List[str]:
+    soup = BeautifulSoup(raw_html, features="html.parser")
 
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action:
-```
-$JSON_BLOB
-```
-Observation: the result of the action
-... (This Thought/Action/Observation will repeat at N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-"""
+    for script in soup(["script", "style"]):
+        script.extract()
 
-SUFFIX = """
-Begin! 
-
-Reminders ALWAYS use the exact characters `Final Answer` when responding with the final answer.
-"""
+    chunks = chunk_element(soup, max_size)
+    chunks = merge_text_chunks(chunks, max_size)
+    return chunks
