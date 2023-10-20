b61d4e42f6f339648ab956186f5c037db8490003
NSR <65774401+JeremyEngram@users.noreply.github.com>
2023-10-20 01:27:54 -0400

    Update llm.py

--- llm_osint/llm.py
+++ llm_osint/llm.py
@@ -1,17 +1,24 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.chat_models.base import BaseChatModel
+from transformers import ChatGPT
 
-LLMModel = BaseChatModel
+# Define default options
+default_fast_llm_options = {
+    "model_name": "gpt-3.5-turbo-16k-0613",
+    "max_response_length": 100,  # Set your desired max response length
+    "temperature": 0.7,
+}
 
-default_fast_llm_options = dict(model_name="gpt-3.5-turbo-16k-0613", request_timeout=120, max_retries=10, temperature=0.7)
-default_llm_options = dict(model_name="gpt-4-0613", request_timeout=120, max_retries=10, temperature=0.7)
+default_llm_options = {
+    "model_name": "gpt-4-0613",
+    "max_response_length": 100,  # Set your desired max response length
+    "temperature": 0.7,
+}
 
-
-def get_default_fast_llm() -> LLMModel:
-    chat = ChatOpenAI(**default_fast_llm_options)
+def get_default_chat_model(options) -> ChatGPT:
+    chat = ChatGPT.from_pretrained(options["model_name"])
+    chat.default_max_response_length = options["max_response_length"]
+    chat.temperature = options["temperature"]
     return chat
 
-
-def get_default_llm() -> LLMModel:
-    chat = ChatOpenAI(**default_llm_options)
-    return chat
+# Create the default chat models
+default_fast_chat_model = get_default_chat_model(default_fast_llm_options)
+default_llm_chat_model = get_default_chat_model(default_llm_options)
