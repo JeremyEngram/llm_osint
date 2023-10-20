3815b703ebd104d8c78c8a5d54b4c728ed08baf3
NSR <65774401+JeremyEngram@users.noreply.github.com>
2023-10-20 14:57:44 -0400

    Update cache_utils.py

--- llm_osint/cache_utils.py
+++ llm_osint/cache_utils.py
@@ -6,20 +6,16 @@ import os
 
 MAX_CACHE_VAL_LEN = 20
 
-cache_options = dict(cache=True, cache_dir="cache")
-
+cache_options = {
+    "cache": True,
+    "cache_dir": "cache"
+}
 
 def cache_func(func: Callable) -> Callable:
-    """
-    Basic cache to save $$$ on API calls.
-
-    Caches based on `str` and `int` args only.
-    """
-
     def wrap(*args, **kwargs):
         os.makedirs(cache_options["cache_dir"], exist_ok=True)
-        args = [*args] + list(kwargs.values())
-        cache_val = re.sub("[^\w\d]", "", repr([arg for arg in args if isinstance(arg, str) or isinstance(arg, int)]))
+        args_str = "".join([str(arg) for arg in args if isinstance(arg, (str, int))] + [str(kwargs.get(key)) for key in kwargs if isinstance(kwargs[key], (str, int))])
+        cache_val = re.sub("[^\w\d]", "", args_str)
         if len(cache_val) > MAX_CACHE_VAL_LEN:
             cache_val = str(int(hashlib.md5(cache_val.encode("utf-8")).hexdigest(), 16))
         cache_key = func.__name__ + "_" + cache_val
