c2f7ec717f72a67a70aa7011f80905ffb659c5ea
NSR <65774401+JeremyEngram@users.noreply.github.com>
2023-10-20 14:59:57 -0400

    Update web_agent.py

--- llm_osint/web_agent.py
+++ llm_osint/web_agent.py
@@ -1,14 +1,7 @@
 from typing import List, Optional
-
-from langchain.agents import Tool
-from langchain.memory.chat_memory import BaseChatMemory
-from langchain.memory import ConversationBufferMemory
-from langchain.agents.agent import AgentExecutor
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
-
+from langchain.agents import Tool, AgentExecutor, initialize_agent, AgentType
+from langchain.memory import BaseChatMemory, ConversationBufferMemory
 from llm_osint import llm, web_agent_constants
-
 
 def build_web_agent(tools: List[Tool], memory: Optional[BaseChatMemory] = None, **custom_agent_kwargs) -> AgentExecutor:
     agent_kwargs = {
@@ -16,9 +9,12 @@ def build_web_agent(tools: List[Tool], memory: Optional[BaseChatMemory] = None, 
         "suffix": web_agent_constants.SUFFIX,
         "format_instructions": web_agent_constants.FORMAT_INSTRUCTIONS,
     }
+    
     if memory is None:
         memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    
     agent_kwargs.update(custom_agent_kwargs)
+    
     agent_chain = initialize_agent(
         tools,
         llm.get_default_llm(),
@@ -27,4 +23,5 @@ def build_web_agent(tools: List[Tool], memory: Optional[BaseChatMemory] = None, 
         memory=memory,
         agent_kwargs=agent_kwargs,
     )
+    
     return agent_chain
